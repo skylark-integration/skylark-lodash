0fa741f243b2a1282fdbaa147cc01eccc2dbfc58
hudaokeji <github@hudaokeji.com>
2019-03-30 11:38:31 +0900

    add original version

---
+++ dist/skylark-lodash/main.js
@@ -0,0 +1,9 @@
+/**
+ * skylark-lodash - A version of lodash.js that ported to running on skylarkjs.
+ * @author Hudaokeji, Inc.
+ * @version v0.9.0
+ * @link https://github.com/skylark-integration/skylark-lodash/
+ * @license MIT
+ */
+define(["./lodash"],function(n){return n});
+//# sourceMappingURL=sourcemaps/main.js.map
---
+++ src/main.js
@@ -0,0 +1,5 @@
+define([
+	"./lodash"
+],function(_){
+	return _;
+});
